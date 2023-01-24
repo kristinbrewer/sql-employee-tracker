8fbf3125c1f2f34caa96dd7eeaa3ef17ec43fc86
Kristin Brewer <kr527106@gmail.com>
2023-01-24 15:02:27 -0500

    made tables in schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeeInfo_db;
+CREATE DATABASE employeeInfo_db;
+
+USE employeeInfo_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id: INT
+
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    department_id: INT,
+    role_id: INT,
+    manager_id: INT ON DELETE SET NULL,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id)
+);
